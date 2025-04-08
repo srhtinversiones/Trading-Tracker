2df3724e723bc0b9e8136e8b26be946a76322570
srhtinversiones <mentorysftt@gmail.com>
2025-04-08 16:22:23 -0300

    buscador

--- script.js
+++ script.js
@@ -44,3 +44,35 @@ function exportarExcel() {
 function exportarPDF() {
   alert("Funcionalidad de exportar a PDF pendiente");
 }
+const paresTrading = [
+  "BTC/USDT", "ETH/USDT", "XRP/USDT", "DOGE/USDT", "SOL/USDT", "ADA/USDT", "DOT/USDT", "LTC/USDT", "LINK/USDT",
+  "SHIB/USDT", "XMR/USDT", "TON/USDT", "AVAX/USDT", "XLM/USDT", "UNI/USDT", "BCH/USDT", "VET/USDT", "MATIC/USDT",
+  "ATOM/USDT", "ALGO/USDT", "FTM/USDT", "EOS/USDT", "XTZ/USDT", "SAND/USDT", "CAKE/USDT", "FLOW/USDT", "GALA/USDT",
+  "MKR/USDT", "QNT/USDT", "GRT/USDT", "LDO/USDT", "IMX/USDT", "STX/USDT", "INJ/USDT", "RUNE/USDT", "SNX/USDT",
+  "AAVE/USDT", "KAVA/USDT", "NEAR/USDT", "FIL/USDT", "HBAR/USDT", "CHZ/USDT", "CRV/USDT", "ENJ/USDT", "ZIL/USDT",
+  "DYDX/USDT", "COMP/USDT", "BAT/USDT", "1INCH/USDT", "ANKR/USDT", "HOT/USDT", "CELR/USDT", "SUSHI/USDT", "YFI/USDT",
+  "BAL/USDT", "BNT/USDT", "OMG/USDT", "LRC/USDT", "ICX/USDT", "ZRX/USDT", "KNC/USDT", "REN/USDT", "STORJ/USDT",
+  "CVC/USDT", "MTL/USDT", "NMR/USDT", "OCEAN/USDT", "UMA/USDT", "BAND/USDT", "RLC/USDT", "PAXG/USDT", "GNO/USDT",
+  "REP/USDT", "MANA/USDT", "WAVES/USDT", "QTUM/USDT", "ONT/USDT", "DASH/USDT", "ZEC/USDT", "XEM/USDT", "ETC/USDT",
+  "THETA/USDT", "KSM/USDT", "EGLD/USDT", "HNT/USDT", "AR/USDT", "ROSE/USDT", "CELO/USDT", "MINA/USDT", "KDA/USDT",
+  "FLUX/USDT", "GLMR/USDT", "ASTR/USDT", "ACA/USDT", "MOVR/USDT", "CSPR/USDT", "VLX/USDT", "SCRT/USDT", "ICX/USDT",
+  "IOST/USDT", "ELF/USDT", "STPT/USDT", "COTI/USDT", "DGB/USDT", "VTHO/USDT", "TFUEL/USDT"
+];
+
+const parSelect = document.getElementById("par");
+
+paresTrading.forEach(par => {
+  const option = document.createElement("option");
+  option.value = par;
+  option.textContent = par;
+  parSelect.appendChild(option);
+});
+
+new TomSelect("#par", {
+  create: false,
+  sortField: {
+    field: "text",
+    direction: "asc"
+  },
+  placeholder: "Buscar par de trading..."
+});
